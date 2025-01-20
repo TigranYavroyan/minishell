b033b03ad91292b18224a8e71a9c7aa13dcae8a7
Hen Aleksanian (Shok.) <henula09106@gmail.com>
2025-01-20 19:16:04 +0400

    fix(shlvl): export and env add

--- srcs/others/shlvl.c
+++ srcs/others/shlvl.c
@@ -6,7 +6,7 @@
 /*   By: healeksa <healeksa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 20:00:11 by healeksa          #+#    #+#             */
-/*   Updated: 2024/11/20 20:52:29 by healeksa         ###   ########.fr       */
+/*   Updated: 2025/01/20 19:05:13 by healeksa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,21 +29,25 @@ static int	level_check(t_value_type value)
 void	shlvl_process(t_minishell_ptr minishell)
 {
 	t_treenode_ptr	found;
+	t_treenode_ptr	found2;
 	int				level;
 
 	found = _find_bst(minishell->env->root, "SHLVL");
-	if (found)
+	found2 = _find_bst(minishell->export->root, "SHLVL");
+	if (found && found2)
 	{
 		level = level_check(found->value);
 		if (level == 1000)
 		{
 			free(found->value);
 			found->value = ft_strdup("");
+			found2->value = ft_strdup("");
 		}
 		else
 		{
 			free(found->value);
 			found->value = ft_itoa(level);
+			found2->value = ft_itoa(level);
 		}
 	}
 	else
